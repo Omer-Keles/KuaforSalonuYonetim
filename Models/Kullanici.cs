4c5345d73b86f959b96a58f8b009d7ce9a42aaed
omerk <omer.keles1@ogr.sakarya.edu.tr>
2024-12-18 23:55:36 +0300

    Log in ve sign up işlemleri için gerekli tabloların modelleri oluşturuldu.

---
+++ Models/Kullanici.cs
@@ -0,0 +1,12 @@
+﻿namespace KuaforSalonuYonetim.Models;
+
+public class Kullanici
+{
+    public int Kullanici_Id { get; set; }
+    public string Kullanici_Adi { get; set; } = null!;
+    public string Kullanici_Soyad { get; set; } = null!;
+    public string Kullanici_Email { get; set; } = null!;
+    public string Kullanici_Telefon { get; set; } = null!;
+    public string Kullanici_Parola { get; set; } = null!;
+    public string Kullanici_Rol { get; set; } = "Normal";
+}
